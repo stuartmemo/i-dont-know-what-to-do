bf47096f9d08ff006081faeee099d2daf96d8357
Stuart Memo <stuartmemo@gmail.com>
2015-01-01 23:47:30 +0000

    added title

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,37 @@
-'use strict';
+(function (window, undefined) {
+    'use strict';
 
-var count = 0;
+    var count = 0;
 
-setInterval(function () {
+    setInterval(function () {
 
-    if (count === 0) {
-        count++;
-        $('.flames')
-            .clone()
-            .fadeIn()
-            .appendTo('.man__leg--left');
-        $('.man').addClass('man--panic');
-    } else if (count === 1) {
-        count++;
-        $('.man__leg--left .flames')
-            .clone()
-            .fadeIn()
-            .appendTo('.man__arm--left');
-        $('.man')
-            .removeClass('man--panic')
-            .addClass('man--panic-more');
-    } else if (count === 2) {
-        count++;
-        $('.man__arm--left .flames')
-            .clone()
-            .fadeIn()
-            .addClass('left-arm-flames')
-            .appendTo('.man__arm--left');
+        if (count === 0) {
+            count++;
+            $('.flames')
+                .clone()
+                .fadeIn()
+                .appendTo('.man__leg--left');
+            $('.man').addClass('man--panic');
+        } else if (count === 1) {
+            count++;
+            $('.man__leg--left .flames')
+                .clone()
+                .fadeIn()
+                .appendTo('.man__arm--left');
+            $('.man')
+                .removeClass('man--panic')
+                .addClass('man--panic-more');
+        } else if (count === 2) {
+            count++;
+            $('.man__arm--left .flames')
+                .clone()
+                .fadeIn()
+                .addClass('left-arm-flames')
+                .appendTo('.man__arm--left');
 
-        $('.man')
-            .removeClass('man--panic-more')
-            .addClass('man--total-panic');
-    }
-}, 5000);
+            $('.man')
+                .removeClass('man--panic-more')
+                .addClass('man--total-panic');
+        }
+    }, 5000);
+})(window);
